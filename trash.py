1a3ab693597fe7121e27b0d1194e79ef47c8b23e
Lenny Meyerson <lennymeyerson@DESKTOP-O0TM5A2.localdomain>
2019-08-22 16:54:49 -0600

    modify Python GUI test message

--- trash.py
+++ trash.py
@@ -23,7 +23,7 @@ root.rowconfigure(0, weight=1)
 confirm = ttk.Button(mainframe, text="Confirm", command=fireScript).grid(column=1, row=3, sticky=W)
 cancel = ttk.Button(mainframe, text="Cancel", command=closeWindow).grid(column=3, row=3, sticky=E)
 
-ttk.Label(mainframe, text="In for Day?").grid(column=2, row=2, sticky=E)
+ttk.Label(mainframe, text="Test this thing out?").grid(column=2, row=2, sticky=E)
 
 for child in mainframe.winfo_children(): child.grid_configure(padx=5, pady=5)
 
